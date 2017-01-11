2aba6fa23ebc96656ce0cf899b2727fe279b962e
Jean-Philippe Braun <eon@patapon.info>
2017-01-11 10:26:45 +0100

    Improve hr test
    - capture on source port
    - add steps to remove the router and check that the ping doesn't work

--- tests/hr/test.sh
+++ tests/hr/test.sh
@@ -6,12 +6,14 @@ source ../../common/common.sh
 source ../../common/runner.sh
 
 check_binary neutron
+check_binary terraform
 
-capture_id=""
-port_id=""
+declare -g capture_id
+declare -g port_id
+declare -g tracking_id
 
 task_default() {
-	runner_sequence setup capture can_ping_backend
+	runner_sequence setup capture can_ping_backend delete_capture delete_router capture cannot_ping_backend
     result=${?}
     runner_sequence teardown 
     return $result
@@ -19,12 +21,15 @@ task_default() {
 
 task_setup() {
     retry 3 terraform apply || return 1
-    port_id=$(resource_id "openstack_networking_port_v2.hr_router_port_bastion") || return 1
+    port_id=$(resource_id "openstack_networking_port_v2.hr_bastion_port") || return 1
+}
+
+task_destroy() {
+    retry 3 terraform destroy -force || return 1
 }
 
 task_teardown() {
-    runner_sequence delete_capture
-    retry 3 terraform destroy -force || return 1
+    runner_parallel delete_capture destroy
 }
 
 task_capture() {
@@ -39,9 +44,40 @@ task_delete_capture() {
 
 task_can_ping_backend() {
 	result=$(gremlin "G.V().Has('Neutron/PortID', '$port_id').Flows().Has('Application', 'ICMPv4')") || return 1
+    tracking_id=$(echo $result | jq -r '.[].TrackingID')
+    if [[ -z $tracking_id ]]; then
+        runner_log_error "No flow found"
+        return 1
+    else
+        runner_log_success "Found expected flow with TrackingID ${tracking_id}"
+    fi
     both_sides=$(echo $result | jq '.[].Metric | has("ABPackets") and has("BAPackets")')
 	if [[ $both_sides == "false" ]] || [[ $both_sides == "" ]]; then
 		runner_log_error "Ping doesn't work. No reply from backend."
 		return 1
+    else
+        runner_log_success "Reply to ping found"
+	fi
+}
+
+task_delete_router() {
+    router_id=$(resource_id "openstack_compute_instance_v2.hr_router") || return 1
+    runner_run nova delete $router_id
+}
+
+task_cannot_ping_backend() {
+    result=$(gremlin "G.V().Has('Neutron/PortID', '$port_id').Flows().Has('Application', 'ICMPv4')") || return 1
+    if [[ $tracking_id != $(echo $result | jq -r '.[].TrackingID') ]]; then
+        runner_log_error "Can't find the flow"
+        return 1
+    else
+        runner_log_success "Found expected flow with TrackingID ${tracking_id}"
+    fi
+    both_sides=$(echo $result | jq '.[].Metric | has("ABPackets") and has("BAPackets")')
+	if [[ $both_sides == "true" ]] || [[ $both_sides == "" ]]; then
+		runner_log_error "Ping still works ?!"
+		return 1
+    else
+        runner_log_success "No reply to ping found"
 	fi
 }
