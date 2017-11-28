210093565705605578056f1a29e40d8c595aa5af
Jean-Philippe Braun <eon@patapon.info>
2017-11-28 17:34:18 +0100

    Rework lb-hm test

---
+++ tests/lb-hm/stack.tf
@@ -0,0 +1,138 @@
+variable instances {
+  default = "2"
+}
+
+resource "openstack_networking_network_v2" "lb_hm_net" {
+  name = "lb_hm_net"
+  admin_state_up = "true"
+  region = "${var.region}"
+}
+
+resource "openstack_networking_subnet_v2" "lb_hm_subnet" {
+  name = "lb_hm_subnet"
+  network_id = "${openstack_networking_network_v2.lb_hm_net.id}"
+  cidr = "10.47.47.0/24"
+  ip_version = 4
+  region = "${var.region}"
+}
+
+resource "openstack_networking_port_v2" "lb_hm_port" {
+  network_id = "${openstack_networking_network_v2.lb_hm_net.id}"
+  admin_state_up = "true"
+  security_group_ids = ["${openstack_compute_secgroup_v2.lb_hm_secgroup.id}"]
+  region = "${var.region}"
+  fixed_ip {
+    subnet_id = "${openstack_networking_subnet_v2.lb_hm_subnet.id}"
+  }
+  count = "${var.instances}"
+}
+
+resource "openstack_compute_secgroup_v2" "lb_hm_secgroup" {
+  region = "${var.region}"
+  name = "lb_hm_secgroup"
+  description = "lb_hm_secgroup"
+  rule {
+    from_port = 80
+    to_port = 80
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+    from_group_id = ""
+  }
+  rule {
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+    from_group_id = ""
+  }
+  rule {
+    from_port = "-1"
+    to_port = "-1"
+    ip_protocol = "icmp"
+    cidr = "0.0.0.0/0"
+    from_group_id = ""
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "lb_hm_secgroup_no_http" {
+  region = "${var.region}"
+  name = "lb_hm_secgroup_no_http"
+  description = "lb_hm_secgroup_no_http"
+  rule {
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+    from_group_id = ""
+  }
+  rule {
+    from_port = "-1"
+    to_port = "-1"
+    ip_protocol = "icmp"
+    cidr = "0.0.0.0/0"
+    from_group_id = ""
+  }
+}
+
+resource "openstack_lb_pool_v1" "lb_hm_pool" {
+  name = "lb_pool"
+  protocol = "HTTP"
+  subnet_id = "${openstack_networking_subnet_v2.lb_hm_subnet.id}"
+  lb_method = "ROUND_ROBIN"
+  region = "${var.region}"
+  monitor_ids = ["${openstack_lb_monitor_v1.lb_hm_monitor.id}"]
+}
+
+resource "openstack_lb_monitor_v1" "lb_hm_monitor" {
+  region = "${var.region}"
+  type = "HTTP"
+  delay = 5
+  timeout = 2
+  max_retries = 3
+  admin_state_up = "true"
+  url_path = "/"
+  http_method = "GET"
+  expected_codes = "200"
+}
+
+resource "openstack_lb_vip_v1" "lb_hm_vip" {
+  name = "lb_hm_vip"
+  subnet_id = "${openstack_networking_subnet_v2.lb_hm_subnet.id}"
+  protocol = "HTTP"
+  port = 80
+  pool_id = "${openstack_lb_pool_v1.lb_hm_pool.id}"
+  region = "${var.region}"
+  admin_state_up = "true"
+}
+
+resource "openstack_lb_member_v1" "lb_member" {
+  pool_id = "${openstack_lb_pool_v1.lb_hm_pool.id}"
+  address = "${openstack_networking_port_v2.lb_hm_port.*.fixed_ip.0.ip_address[count.index]}"
+  port = 80
+  region = "${var.region}"
+  admin_state_up = "true"
+  count = "${var.instances}"
+}
+
+resource "openstack_compute_instance_v2" "lb_hm_backend" {
+  region = "${var.region}"
+  name = "lb_hm_backend_${count.index}"
+  image_id = "${var.image_id}"
+  flavor_id = "${var.flavor_id}"
+  network {
+    port = "${openstack_networking_port_v2.lb_hm_port.*.id[count.index]}"
+  }
+  key_pair = "${var.key_pair}"
+  user_data = "${file("userdata-backend.yml")}"
+  count = "${var.instances}"
+}
+
+resource "openstack_networking_floatingip_v2" "lb_hm_fip_vip" {
+  region = "${var.region}"
+  pool = "public"
+  port_id = "${openstack_lb_vip_v1.lb_hm_vip.port_id}"
+}
+
+output "lb_hm_vip_ip" {
+  value = "${openstack_networking_floatingip_v2.lb_hm_fip_vip.address}"
+}
---
+++ tests/lb-hm/test.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+set -e
+
+source ../../common/common.sh
+source ../../common/runner.sh
+
+task_default() {
+    runner_sequence setup wait_backends start_traffic disable_vm1 stop_traffic check_traffic
+    result=${?}
+    runner_sequence teardown
+    return $result
+}
+
+task_setup() {
+    terrapply || return 1
+}
+
+task_wait_backends() {
+    runner_parallel wait_backend_0 wait_backend_1
+}
+
+task_wait_backend_0() {
+    wait_cloudinit lb_hm_backend.0
+}
+
+task_wait_backend_1() {
+    wait_cloudinit lb_hm_backend.1
+}
+
+task_destroy() {
+    terradestroy || return 1
+}
+
+task_teardown() {
+    runner_parallel destroy
+    clean_vars
+}
+
+gen_trafic() {
+    `get_vars`
+    while ${generate}
+    do
+        `get_vars`
+        curl --connect-timeout 0.5 ${1} >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            success=$((success+1))
+        else
+            errors=$((errors))
+        fi
+        save_vars success errors
+    done
+}
+
+task_start_traffic() {
+    fip=$(fip_ip "lb_hm_fip_vip") || return 1
+    success=0
+    errors=0
+    generate=true
+    save_vars generate success errors
+    gen_trafic ${fip} &
+    sleep 2
+}
+
+task_disable_vm1() {
+    port_id=$(resource_id "openstack_networking_port_v2.lb_hm_port.0") || return 1
+    runner_run neutron port-update --security-group lb_hm_secgroup_no_http ${port_id}
+}
+
+task_stop_traffic() {
+    generate=false
+    save_vars generate
+    sleep 5
+}
+
+task_check_traffic() {
+    `get_vars`
+    if [ $errors -gt 1 ]; then
+        runner_log_error "$errors requests failed ($success ok)"
+    else
+        runner_log_success "$errors requests failed ($success ok)"
+    fi
+}
